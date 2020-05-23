d65719f8274754d60c34db1d27619439bc2b6972
benny poliak <benderu.m@gmail.com>
2020-05-23 19:36:06 +0100

    update repo

--- src/API.js
+++ src/API.js
@@ -2,8 +2,26 @@ const API_ENDPOINT = "http://localhost:3000"
 const USERS_URL = `${API_ENDPOINT}/users?_embed=tweets`
 const TWEETS_URL = `${API_ENDPOINT}/tweets?_expand=user&_embed=comments`
 
-const getTweets = () => fetch(TWEETS_URL).then(res => res.json())
+const getTweets = () => fetch(TWEETS_URL).then(res => res.json());
+
+
+
+
+export const users = [];
+
+const getUsers = async() => {
+    const userObj = await fetch("http://localhost:3000/users").then((response) => response.json());
+    const userNames = userObj.map(el => {
+        return el.name;
+    })
+    userNames.forEach(el => users.push(el));
+
+    return userNames;
+}
+
+getUsers();
 
 export default {
-    getTweets
+    getTweets,
+
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import API from './API'
+import './scss/style.scss';
+import API, { users } from './API.js';
+const userNAme = document.querySelector("#userName");
+const loginBtn = document.querySelector(".loginBtn");
 
-// Your code here
+loginBtn.addEventListener("click", () => {
+    event.preventDefault();
+    console.log(userNAme.value)
 
-API.getTweets()
+    if (users.includes(userNAme.value)) {
+        console.log("user exists")
+        window.open("home.html", "_self");
+    } else {
+        console.log(users);
+        console.log("clicked!")
+    }
+})
